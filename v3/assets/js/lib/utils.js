ecd6374561e58039e3b0a9992f0663b392bf41b2
MightyMike001 <mikegulien1990@gmail.com>
2025-09-12 11:11:15 +0200

    Delete v3 directory

--- v3/assets/js/lib/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(s){
-  const d = new Date(s);
-  return isNaN(d) ? '' : d.toLocaleDateString('nl-NL');
-}
-export function getTodayString(){
-  return new Date().toISOString().split('T')[0];
-}
